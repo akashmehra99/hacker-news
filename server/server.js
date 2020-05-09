68a5c783b3f836e26f69edbf12ccf3dcfa3102f9
Akash MEHRA <akash.mehra99@gmail.com>
2020-05-10 00:47:49 +0530

    Initial Code setup and base requirements for the app

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'dist');
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
@@ -11,4 +11,4 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'))
 });
 
-app.listen(port, () => console.log('server is up'));
+app.listen(port, () => console.log('server is up,', process.env.PORT, process.env.port));
