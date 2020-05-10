4298c72dece0496bc0ba4d87ec56d305e1249103
Akash MEHRA <akash.mehra99@gmail.com>
2020-05-10 13:37:43 +0530

    Code changes for upvote and hide logic

--- src/components/Items.js
+++ src/components/Items.js
@@ -4,13 +4,23 @@ import "../styles/components-styles/item.scss";
 class Items extends React.Component {
   constructor(props) {
     super(props);
+    this.setHide = this.setHide.bind(this);
+    this.upvote = this.upvote.bind(this);
+  }
+
+  setHide =() => {
+    this.props.hide(this.props.item.story_id);
+  }
+
+  upvote = () => {
+    this.props.upvote(this.props.item.story_id);
   }
 
   render() {
     return (
       <div className="item_container">
         <div className="index">{this.props.index}.</div>
-        <div className="upvote"></div>
+        <button className="upvote_button" onClick={this.upvote} ><div className="upvote"></div></button>
         <div className="item_title_container">
           <div className="item_title">
             <div>
@@ -32,8 +42,8 @@ class Items extends React.Component {
             <span> by {this.props.item.author} </span>
 
             <span>{this.props.item.timeDifference} | </span>
-            <span>hide | </span>
-            <span>{this.props.item.num_comments || 0} comments</span>
+            <button className="hide" onClick={this.setHide}>hide</button>
+            <span> | {this.props.item.num_comments || 0} comments</span>
           </div>
         </div>
       </div>
--- src/components/List.js
+++ src/components/List.js
@@ -1,37 +1,66 @@
 import React from "react";
-import moment from 'moment';
+import moment from "moment";
 
-import '../styles/components-styles/list.scss';
-import Items from './Items';
+import "../styles/components-styles/list.scss";
+import Items from "./Items";
 
 class List extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      pageNumber: localStorage.getItem("pageNumber") || 0,
+      pageNumber: 0,
       hits: [],
       error: false,
-      startIndex: 1
+      startIndex: 1,
     };
     this.loadNext = this.loadNext.bind(this);
+    this.hideStory = this.hideStory.bind(this);
+    this.upVote = this.upVote.bind(this);
   }
 
   componentDidMount() {
+    localStorage.setItem("pageNumber", 0);
     this.getDataFromApi(this.state.pageNumber);
   }
 
-  loadNext = () => {
-      const next = +localStorage.getItem('pageNumber') + 1;
-      localStorage.setItem('pageNumber', next);
-      this.getDataFromApi(next);
+  hideStory = (story_id) => {
+    const filtered = this.state.hits.filter(
+      (item) => item.story_id !== story_id
+    );
+    this.state.startIndex = this.state.pageNumber * 20 + 1;
+    this.setState(() => ({ hits: filtered }));
+    // Code for api call to hide
+  };
+
+  upVote = (story_id) => {
+    const hits = this.state.hits;
+    for (let i = 0; i < hits.length; i++) {
+      if (hits[i].story_id === story_id) {
+        hits[i].points = (hits[i].points || 0) + 1;
+        break;
+      }
+    }
+    this.state.startIndex = (this.state.pageNumber * 20) + 1;
+    this.setState(() => ({hits}));
+    // api call for upVotes code goes here
   }
+
+  loadNext = () => {
+    const next = +localStorage.getItem("pageNumber") + 1;
+    localStorage.setItem("pageNumber", next);
+    this.getDataFromApi(next);
+  };
 
   getDataFromApi(pageNumber) {
     fetch(`https://hn.algolia.com/api/v1/search_by_date?page=${pageNumber}`)
       .then((res) => res.json())
       .then(
         (result) => {
-          this.setState(() => ({ hits: result.hits, error: false, pageNumber }));
+          this.setState(() => ({
+            hits: result.hits,
+            error: false,
+            pageNumber,
+          }));
           window.scrollTo(0, 0);
         },
         (error) => {
@@ -43,37 +72,48 @@ class List extends React.Component {
     const a = moment();
     const b = moment(time);
     if (a.diff(b, "years") > 0) {
-        return a.diff(b, "years") + ' year ago';
+      return a.diff(b, "years") + " year ago";
     } else if (a.diff(b, "months") > 0) {
-        return a.diff(b, "months") + ' month ago';
+      return a.diff(b, "months") + " month ago";
     } else if (a.diff(b, "weeks") > 0) {
-        return a.diff(b, "weeks") + ' week ago';
+      return a.diff(b, "weeks") + " week ago";
     } else if (a.diff(b, "days") > 0) {
-        return a.diff(b, "days") + ' day ago';
+      return a.diff(b, "days") + " day ago";
     } else if (a.diff(b, "hours") > 0) {
-        return a.diff(b, "hours") + ' hour ago';
+      return a.diff(b, "hours") + " hour ago";
     } else if (a.diff(b, "minutes") > 0) {
-        return a.diff(b, "minutes") + ' minute ago';
-    } else  {
-        return a.diff(b, "seconds") + ' seconds ago';
+      return a.diff(b, "minutes") + " minute ago";
+    } else {
+      return a.diff(b, "seconds") + " seconds ago";
     }
   }
   render() {
     return (
+      <React.Fragment>
         <React.Fragment>
-            <React.Fragment>
-                {this.state.hits.length > 0  && this.state.hits.map((item, index) =>{
-                    item.domain = item.story_url ? `(${item.story_url.split('/')[2]})`: null;
-                    item.timeDifference = this.getTimeDifference(item.created_at);
-                    return <Items key={index} item={item} index={this.state.startIndex++} />  
-                })}
-            </React.Fragment>
-            <React.Fragment>
-                <div onClick={this.loadNext} className="more">
-                    More
-                </div>
-            </React.Fragment>
-        </React.Fragment>   
+          {this.state.hits.length > 0 &&
+            this.state.hits.map((item, index) => {
+              item.domain = item.story_url
+                ? `(${item.story_url.split("/")[2]})`
+                : null;
+              item.timeDifference = this.getTimeDifference(item.created_at);
+              return (
+                <Items
+                  key={index}
+                  item={item}
+                  index={this.state.startIndex++}
+                  upvote={(story_id) => this.upVote(story_id)}
+                  hide={(story_id) => this.hideStory(story_id)}
+                />
+              );
+            })}
+        </React.Fragment>
+        <React.Fragment>
+          <div onClick={this.loadNext} className="more">
+            More
+          </div>
+        </React.Fragment>
+      </React.Fragment>
     );
   }
 }
